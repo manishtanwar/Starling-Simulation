3e3751532d8c0eb7cdd43b7b745a3fbee5c0fea4
Abhinash <abhinash2k16@gmail.com>
2018-04-29 00:28:53 +0530

    changes in boids.cpp

--- Boids/boids.cpp
+++ Boids/boids.cpp
@@ -12,54 +12,54 @@ void drawCoordinates(){
     glBegin(GL_LINES);
     // x aix
  
-    glVertex3f(-0.30, 0.0f, 0.0f);
-    glVertex3f(0.30, 0.0f, 0.0f);
+    glVertex3f(-0.5, 0.0f, 0.0f);
+    glVertex3f(0.5, 0.0f, 0.0f);
  
-    glVertex3f(0.3, 0.0f, 0.0f);
-    glVertex3f(0.25, 0.05f, 0.0f);
+    glVertex3f(0.5, 0.0f, 0.0f);
+    glVertex3f(0.45, 0.05f, 0.0f);
  
-    glVertex3f(0.3, 0.0f, 0.0f);
-    glVertex3f(0.25, -0.05f, 0.0f);
+    glVertex3f(0.5, 0.0f, 0.0f);
+    glVertex3f(0.45, -0.05f, 0.0f);
 
-    glVertex3f(-0.3, 0.0f, 0.0f);
-    glVertex3f(-0.25, -0.05f, 0.0f);
+    glVertex3f(-0.5, 0.0f, 0.0f);
+    glVertex3f(-0.45, -0.05f, 0.0f);
  
-    glVertex3f(-0.3, 0.0f, 0.0f);
-    glVertex3f(-0.25, 0.05f, 0.0f);
+    glVertex3f(-0.5, 0.0f, 0.0f);
+    glVertex3f(-0.45, 0.05f, 0.0f);
     glEnd();
     // glFlush();
  
     // y 
     glColor3f(0.0,1.0,0.0); // green y
     glBegin(GL_LINES);
-    glVertex3f(0.0, -0.3f, 0.0f);
-    glVertex3f(0.0, 0.3f, 0.0f);
+    glVertex3f(0.0, -0.5f, 0.0f);
+    glVertex3f(0.0, 0.5f, 0.0f);
  
-    glVertex3f(0.0, 0.3f, 0.0f);
-    glVertex3f(0.05, 0.25f, 0.0f);
+    glVertex3f(0.0, 0.5f, 0.0f);
+    glVertex3f(0.05, 0.45f, 0.0f);
  
-    glVertex3f(0.0, 0.3f, 0.0f);
-    glVertex3f(-0.05, 0.25f, 0.0f);
+    glVertex3f(0.0, 0.5f, 0.0f);
+    glVertex3f(-0.05, 0.45f, 0.0f);
 
-    glVertex3f(0.0, -0.3f, 0.0f);
-    glVertex3f(0.05, -0.25f, 0.0f);
+    glVertex3f(0.0, -0.5f, 0.0f);
+    glVertex3f(0.05, -0.45f, 0.0f);
  
-    glVertex3f(0.0, -0.3f, 0.0f);
-    glVertex3f(-0.05, -0.25f, 0.0f);
+    glVertex3f(0.0, -0.5f, 0.0f);
+    glVertex3f(-0.05, -0.45f, 0.0f);
     glEnd();
  
     // z 
     // glColor3f(0.0,0.0,1.0); // blue z
     // glBegin(GL_LINES);
-    // glVertex3f(0.0, 0.0f ,-0.3f );
-    // glVertex3f(0.0, 0.0f ,0.3f );
+    // glVertex3f(0.0, 0.0f ,-0.5f );
+    // glVertex3f(0.0, 0.0f ,0.5f );
  
  
-    // glVertex3f(0.0, 0.0f ,0.3f );
-    // glVertex3f(0.0, 1.0f ,0.3f );
+    // glVertex3f(0.0, 0.0f ,0.5f );
+    // glVertex3f(0.0, 1.0f ,0.5f );
  
-    // glVertex3f(0.0, 0.0f ,0.3f );
-    // glVertex3f(0.0, -1.0f ,0.3f );
+    // glVertex3f(0.0, 0.0f ,0.5f );
+    // glVertex3f(0.0, -1.0f ,0.5f );
     // glEnd();
  
 }
@@ -71,15 +71,15 @@ void drawCoordinates(){
 
 // *
 // draw white polygon (rectangle) with corners at
-// (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0)
+// (0.45, 0.45, 0.0) and (0.75, 0.75, 0.0)
 // http://heron.cc.ukans.edu/ebt-bin/nph-dweb/dyna...G/@Generic__BookTextView/622;cs=fullhtml;pt=532 (10 of 16) [4/28/2000 9:44:16 PM]OpenGL Programming Guide (Addison-Wesley Publishing Company)
 
 // // glColor3f (1.0, 1.0, 0.0);
 // // glBegin(GL_POLYGON);
-// // glVertex3f (0.0, 0.25,0.0);
-// // glVertex3f (0.75, 0.25,0.0);
+// // glVertex3f (0.0, 0.45,0.0);
+// // glVertex3f (0.75, 0.45,0.0);
 // // glVertex3f (0.75, 0.75,0.0);
-// // glVertex3f (0.25, 0.75,0.0);
+// // glVertex3f (0.45, 0.75,0.0);
 // // glEnd();
 
 // drawCoordinates();
@@ -100,7 +100,7 @@ void display(void)
   // glRotatef(rs.cameraX, 1, 0, 0);
   // glRotatef(rs.cameraY, 0, 1, 0);
  
-  // glScalef (0.3, 0.3, 0.3);
+  glScalef (0.3, 0.3, 0.3);
   // glutWireCube (1.0);
   drawCoordinates();
  
