816108e6569938eed2ec94bb13357bc9a3266546
Manish <manish.tanwar19@gmail.com>
2018-04-29 13:14:24 +0530

    interface all set

--- src/main.cpp
+++ src/main.cpp
@@ -4,127 +4,90 @@
 #include <GL/glut.h>
 // #include <Transform.h>
 
+int rotx=0,roty=0,rotz =0;
+float transx = 0,transy = 0,transz = 0,scale = 0.001;
+int w=1000,h=1000;
+int ta = 0;
 
-void drawCoordinates(){
+void drawCoordinates()
+{
  
     // draw some lines
-    glLineWidth(1.2);
-    glColor3f(1.0,0.0,0.0); // red x
+    glLineWidth(0.5);
+    glColor3f(1,0,0); // red x
     glBegin(GL_LINES);
     // x aix
  
-    glVertex3f(-0.5, 0.0f, 0.0f);
-    glVertex3f(0.5, 0.0f, 0.0f);
+    glVertex3f(-50, 0, 0);
+    glVertex3f(50, 0, 0);
  
-    glVertex3f(0.5, 0.0f, 0.0f);
-    glVertex3f(0.45, 0.05f, 0.0f);
+    glVertex3f(50, 0, 0);
+    glVertex3f(45, 5, 0);
  
-    glVertex3f(0.5, 0.0f, 0.0f);
-    glVertex3f(0.45, -0.05f, 0.0f);
+    glVertex3f(50, 0, 0);
+    glVertex3f(45, -5, 0);
 
-    // glVertex3f(-0.5, 0.0f, 0.0f);
-    // glVertex3f(-0.45, -0.05f, 0.0f);
- 
-    // glVertex3f(-0.5, 0.0f, 0.0f);
-    // glVertex3f(-0.45, 0.05f, 0.0f);
     glEnd();
-    // glFlush();
  
     // y 
-    glColor3f(0.0,1.0,0.0); // green y
+    glColor3f(0,1,0); // green y
     glBegin(GL_LINES);
-    glVertex3f(0.0, -0.5f, 0.0f);
-    glVertex3f(0.0, 0.5f, 0.0f);
+    glVertex3f(0, -50, 0);
+    glVertex3f(0, 50, 0);
  
-    glVertex3f(0.0, 0.5f, 0.0f);
-    glVertex3f(0.05, 0.45f, 0.0f);
+    glVertex3f(0, 50, 0);
+    glVertex3f(5, 45, 0);
  
-    glVertex3f(0.0, 0.5f, 0.0f);
-    glVertex3f(-0.05, 0.45f, 0.0f);
+    glVertex3f(0, 50, 0);
+    glVertex3f(-5, 45, 0);
 
-    // glVertex3f(0.0, -0.5f, 0.0f);
-    // glVertex3f(0.05, -0.45f, 0.0f);
- 
-    // glVertex3f(0.0, -0.5f, 0.0f);
-    // glVertex3f(-0.05, -0.45f, 0.0f);
     glEnd();
  
     // z 
-    glColor3f(0.0,0.0,1.0); // blue z
+    glColor3f(0,0,1); // blue z
     glBegin(GL_LINES);
-    glVertex3f(0.0, 0.0f ,-0.5f );
-    glVertex3f(0.0, 0.0f ,0.5f );
+    glVertex3f(0, 0, -50);
+    glVertex3f(0, 0, 50);
  
  
-    glVertex3f(0.0, 0.0f ,0.5f );
-    glVertex3f(0.0, 0.05f ,0.45f );
+    glVertex3f(0, 0, 50);
+    glVertex3f(0, 5, 45);
  
-    glVertex3f(0.0, 0.0f ,0.5f );
-    glVertex3f(0.0, -0.05f ,0.45f );
+    glVertex3f(0, 0, 50);
+    glVertex3f(0, -5, 45);
 
-    // glVertex3f(0.0, 0.0f ,-0.5f );
-    // glVertex3f(0.0, 0.05f ,-0.45f );
- 
-    // glVertex3f(0.0, 0.0f ,-0.5f );
-    // glVertex3f(0.0, -0.05f ,-0.45f );
     glEnd();
  
 }
 
-// void display()
-// {
-// /* clear all pixels */
-// glClear (GL_COLOR_BUFFER_BIT);
-
-// *
-// draw white polygon (rectangle) with corners at
-// (0.45, 0.45, 0.0) and (0.75, 0.75, 0.0)
-// http://heron.cc.ukans.edu/ebt-bin/nph-dweb/dyna...G/@Generic__BookTextView/622;cs=fullhtml;pt=532 (10 of 16) [4/28/2000 9:44:16 PM]OpenGL Programming Guide (Addison-Wesley Publishing Company)
-
-// // glColor3f (1.0, 1.0, 0.0);
-// // glBegin(GL_POLYGON);
-// // glVertex3f (0.0, 0.45,0.0);
-// // glVertex3f (0.75, 0.45,0.0);
-// // glVertex3f (0.75, 0.75,0.0);
-// // glVertex3f (0.45, 0.75,0.0);
-// // glEnd();
-
-// drawCoordinates();
-
-
-// /* don't wait!
-// * start processing buffered OpenGL routines
-// */
-// // glFlush ();
-// }
-int rotx=0,roty=0,rotz =0;
-float transx = 0,transy = 0,scale = 1;
-
-
 void display(void)
 {
-  glClear (GL_COLOR_BUFFER_BIT);
-  glColor3f (1.0, 1.0, 1.0);
-  glLoadIdentity ();
-  
-  glTranslatef(transx, 0, 0);
-  glTranslatef(0, transy, 0);
-  // glTranslatef(0, 0, transz);
-  
-  // glRotatef(rs.cameraX, 1, 0, 0);
-  // glTranslatef(transx, transy, transz);
-  glRotatef(rotx, 1, 0, 0);
-  glRotatef(roty, 0, 1, 0);
-  glRotatef(rotz, 0, 0, 1);
-  gluLookAt (0.1, 0.1, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
-  // glScalef (0.3, 0.3, 0.3);
-  glScalef (0.3*scale, 0.3*scale, 0.3*scale);
-  // glutWireCube (1.0);
-  drawCoordinates();
-  // gluLookAt (5.0, 5.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
- 
-  glFlush ();
+    glClear (GL_COLOR_BUFFER_BIT);
+    glColor3f (1.0, 1.0, 1.0);
+    glLoadIdentity ();
 
+    gluLookAt (0, 0, 0.1, 0, 0, 0, 0, 1, 0);
+    glScalef (0.3*scale, 0.3*scale, 0.3*scale);
+
+    glTranslatef(transx, 0, 0);
+    glTranslatef(0, transy, 0);
+    glTranslatef(0, 0, transz);
+
+    glRotatef(rotx, 1, 0, 0);
+    glRotatef(roty, 0, 1, 0);
+    glRotatef(rotz, 0, 0, 1);
+
+    glColor3f(1,0,1);
+    
+    glLineWidth(0.5);
+    glBegin(GL_POLYGON);
+    glVertex3f(5+ta, 5, 0);
+    glVertex3f(5+ta, -5, 0);
+    glVertex3f(10+ta, 0, 0);
+    glEnd();
+
+    drawCoordinates();
+    glFlush ();
 }
  
 
@@ -134,8 +97,8 @@ void keyboard_action(unsigned char key, int x,int y)
     {
         case 'r' :
                 rotx = roty = rotz = 0;
-                transx = transy  = 0.0;
-                scale = 1.0;
+                transx = transy = transz  = 0.0;
+                scale = 0.001;
                 glutPostRedisplay();
                 break;
         case 'x' :
@@ -151,21 +114,30 @@ void keyboard_action(unsigned char key, int x,int y)
                 glutPostRedisplay();
                 break;
         case 'd' :
-                transx = (transx+0.05);
+                transx = (transx + 5);
                 glutPostRedisplay();
                 break;
         case 'w' :
-                transy = (transy+0.05);
+                transy = (transy + 5);
                 glutPostRedisplay();
                 break;
+        case 'e' :
+                transz = (transz + 5);
+                glutPostRedisplay();
+                break;        
         case 'a' :
-                transx = (transx-0.05);
+                transx = (transx - 5);
                 glutPostRedisplay();
                 break;
         case 's' :
-                transy = (transy-0.05);
+                transy = (transy - 5);
                 glutPostRedisplay();
                 break;
+        case 'q' :
+                transz = (transz - 5);
+                glutPostRedisplay();
+                break;
+
         default :
                 break;
     }
@@ -189,45 +161,66 @@ void specialKey_action(int key, int x, int y)
 	}
 }
 
+void reshape(int width,int height)
+{
+    // glViewport(0,0,widht,height);
+    w = width;
+    h = height;
+    glViewport (0, 0, w,h);
 
+    glMatrixMode (GL_PROJECTION);
+    // glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluPerspective(60.0, (float)w/h, 0.0001f, 100.0f);
 
-
+    // glMatrixMode (GL_PROJECTION);
+    // glLoadIdentity ();
+    // gluPerspective(60.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
+    glMatrixMode (GL_MODELVIEW);
+}
 
 
 void init ()
 {
- // select clearing (background) color
-glClearColor (0.0, 0.0, 0.0, 0.0);
-/*
+     // select clearing (background) color
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    /*
 
-// initialize viewing values */
-glMatrixMode(GL_MODELVIEW);
-glLoadIdentity();
+    // // initialize viewing values */
+    glMatrixMode (GL_PROJECTION);
+    // glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluPerspective(60.0, (float)w/h, 0.0001f, 100.0f);
 
-glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+    // glMatrixMode (GL_PROJECTION);
+    // glMatrixMode (GL_MODELVIEW);
+    // glLoadIdentity ();
+    // gluPerspective(60.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
+    glMatrixMode (GL_MODELVIEW);
+    // glLoadIdentity ();
+    // glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 }
-/*
-*
-*
-*
-*
-*
-*/
+
+void go()
+{
+    ta += 1;
+}
+
 int main(int argc, char** argv)
 {
-glutInit(&argc, argv);
-glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-glutInitWindowSize (1000, 1000);
-glutInitWindowPosition (50, 50);
-glutCreateWindow ("boids");
-init ();
-glutDisplayFunc(display);
-glutSpecialFunc(specialKey_action);
-glutKeyboardFunc(keyboard_action);
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize (1000, 1000);
+    glutInitWindowPosition (50, 50);
+    glutCreateWindow ("boids");
+    init ();
+    glutDisplayFunc(display);
+    glutSpecialFunc(specialKey_action);
+    glutKeyboardFunc(keyboard_action);
+    go();
+    glutReshapeFunc(reshape);
+    glutReshapeWindow(w, h);
 
-
-// void glutSolidSphere(GLdouble 0.5, GLint 1000, GLint 1);
-glutMainLoop();
-return 0;
-/* ISO C requires main to return int. */
+    glutMainLoop();
+    return 0;
 }
